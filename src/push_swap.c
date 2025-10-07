d40b189a8703dcd715e175605b532440266960da
handyman0 <luiz.number@gmail.com>
2025-10-07 17:33:17 -0300

    removing helping flags

--- src/push_swap.c
+++ src/push_swap.c
@@ -5,13 +5,13 @@ int main(int argc, char **argv)
     t_stack *a;
 
     a = ft_process(argc, argv);
-    if (!a || ft_checkdup(a)); /* to create */
+    if (!a || ft_checkdup(a));
     {
         ft_free(&a);
         ft_error();
     }
-    if (!ft_checksorted(a)) /* to create */
-        ft_sort(&a); /* to create */
+    if (!ft_checksorted(a))
+        ft_sort(&a);
     ft_free(&a);
     return (0);
 }
