7c0b764bfe464f32e0a54c2c3988fb16e880ca20
handyman0 <luiz.number@gmail.com>
2025-10-09 17:50:00 -0300

    feat: implement rotate and swap functions for stack 'b'

---
+++ src/operations3.c
@@ -0,0 +1,34 @@
+#include "../include/push_swap.h"
+
+/*  rb (rotate b) - sobe cada elemento da pilha "b" para cima.
+	o primeiro vira o ultimo */
+void    ft_rb(t_stack **b, int j)
+{
+	t_stack *tmp;
+	
+	if (!*b || !(*b)->next)
+		return ;
+	tmp = *b;
+	*b = ft_lstlast(*b);
+	(*b)->next = tmp;
+	*b = tmp->next;
+	tmp->next = NULL;
+	if(j == 0)
+		write(1, "rb\n", 3);
+}
+
+/* sb (swap b) troca a posição dos dois elementos de cima da pilha
+	não faz nada se for 1 ou nenhum elemento */
+void	ft_sb(t_stack **b, int j)
+{
+	t_stack *tmp;
+
+	if (!*b || !(*b)->next)
+		return ;
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = (*b)->next;
+	(*b)->next = tmp;
+	if (j == 0)
+		write(1, "sb\n", 3);
+}
