f9c5f774212538bab9b2dc828df361aa265f3476
handyman0 <luiz.number@gmail.com>
2025-10-14 16:40:29 -0300

    changging to other file

--- src/checker_utils.c
+++
@@ -1,7 +0,0 @@
-#include "../include/push_swap.h"
-
-void	ft_error_ch(void)
-{
-	write(2, "Error\n", 6);
-	exit(EXIT_FAILURE);
-}
--- src/ft_error.c
+++
@@ -1,7 +0,0 @@
-#include "../include/push_swap.h"
-
-void    ft_error(void)
-{
-    write(2, "Error\n", 6);
-    exit(1);
-}
