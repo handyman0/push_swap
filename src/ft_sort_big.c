bee43b9d6692ececab7e7388ca632db3cce8df21
handyman0 <luiz.number@gmail.com>
2025-10-14 15:54:01 -0300

    style: format code and comments for consistency in ft_sort_big.c

--- src/ft_sort_big.c
+++ src/ft_sort_big.c
@@ -1,10 +1,10 @@
 #include "../include/push_swap.h"
 
-/*  Essa função classifica e empurra pilhas até que 
+/*	Essa função classifica e empurra pilhas até que
 	restem 3 elementos na pilha A */
-void    ft_sort_b_till_3(t_stack **stack_a, t_stack **stack_b)
+void	ft_sort_b_till_3(t_stack **stack_a, t_stack **stack_b)
 {
-	int     i;
+	int		i;
 	t_stack *tmp;
 
 	while (ft_lstsize(*stack_a) > 3 && !ft_checksorted(*stack_a))
@@ -18,24 +18,24 @@ void    ft_sort_b_till_3(t_stack **stack_a, t_stack **stack_b)
 			else if (i == ft_case_rrarrb(*stack_a, *stack_b, tmp->nbr))
 				i = ft_apply_rrarrb(stack_a, stack_b, tmp->nbr, 'a');
 			else if (i == ft_case_rarrb(*stack_a, *stack_b, tmp->nbr))
-                i = ft_apply_rarrb(stack_a, stack_b, tmp->nbr, 'a');
+				i = ft_apply_rarrb(stack_a, stack_b, tmp->nbr, 'a');
 			else if (i == ft_case_rrarb(*stack_a, *stack_b, tmp->nbr))
-                i = ft_apply_rrarb(stack_a, stack_b, tmp->nbr, 'a');
+				i = ft_apply_rrarb(stack_a, stack_b, tmp->nbr, 'a');
 			else
 				tmp = tmp->next;
 		}
 	}
 }
-/*	Essa função empurra, um por um, todos os elementos da pilha_a 
+/*	Essa função empurra, um por um, todos os elementos da pilha_a
 	para a pilha_b, até que restem até apenas tres elementos na pilha_a.
 	Ao empurrar, ela garante que a pilha_b esteja ordenada.
-	Quando restam três elementos, ela chama a função ft_sort_three 
+	Quando restam três elementos, ela chama a função ft_sort_three
 	para ordenar os elementos restantes na pilha_a.
 */
 t_stack	*ft_sort_b(t_stack **stack_a)
 {
 	t_stack *stack_b;
-	
+
 	stack_b = NULL;
 	if (ft_lstsize(*stack_a) > 3 && !ft_checksorted(*stack_a))
 		ft_pb(stack_a, &stack_b, 0);
@@ -77,7 +77,7 @@ t_stack	**ft_sort_a(t_stack **stack_a, t_stack **stack_b)
 	E finalmente, realiza a ordenação final na pilha A após todos os elementos
 	serem enviados para a pilha B, ordenados e enviados de volta para a pilha A.
 	Porque, mesmo que a pilha A seja ordenada no final, o numero minimo deve estar
-	no topo da pilha A. 
+	no topo da pilha A.
 	Portanto, ela simplesmente traz o menor numero da pilha A para o topo */
 void	ft_sort(t_stack **stack_a)
 {
